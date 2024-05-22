0446040f5cef7b9ebb3ebef51ed5ac9973563d26
apathriel <gabrielandersen1@gmail.com>
2024-05-22 10:39:51 +0200

    Update env name. Remove pointer to cds-visual.

--- assignments/transfer_learning_pretrained_cnns/setup_win.sh
+++ assignments/transfer_learning_pretrained_cnns/setup_win.sh
@@ -2,9 +2,9 @@
 
 python -m pip install --user virtualenv
 
-python -m virtualenv va03_env
+python -m virtualenv env
 
-source ./va03_env/Scripts/activate
+source ./env/Scripts/activate
 
 pip install --upgrade pip
 pip install -r requirements.txt
